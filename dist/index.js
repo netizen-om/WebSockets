36882019858bd80ac487289c2a27490cb39ea549
netizen-om <borisagarom@gmail.com>
2025-05-26 15:10:45 +0530

    fix

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8000 });
 wss.on("connection", (socket) => {
+    console.log("hello");
+    setInterval(() => {
+        socket.send("current price of SOLANA is : " + Math.random());
+    }, 1000);
+    socket.on("message", (e) => {
+        console.log(e.toString());
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ const wss = new WebSocketServer({ port : 8000 })
 
 wss.on("connection", (socket) => {
     console.log("hello");
-    socket.send("Hello, thx for connecting");
-    
+    setInterval(() => {
+        socket.send("current price of SOLANA is : " + Math.random());
+        
+    }, 1000)
+
+    socket.on("message", (e) => {
+        console.log(e.toString());
+        
+    })
+
+
 })
