e164dcd2f5903cc8c272a71afbcc52b34ebada78
netizen-om <borisagarom@gmail.com>
2025-05-26 15:16:22 +0530

    added react

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8000 });
 wss.on("connection", (socket) => {
-    console.log("hello");
-    setInterval(() => {
-        socket.send("current price of SOLANA is : " + Math.random());
-    }, 1000);
+    console.log("user connectd");
     socket.on("message", (e) => {
         console.log(e.toString());
+        console.log(e.toString() == "ping");
+        if (e.toString() == "ping") {
+            socket.send("pong");
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port : 8000 })
 
 wss.on("connection", (socket) => {
-    console.log("hello");
-    setInterval(() => {
-        socket.send("current price of SOLANA is : " + Math.random());
-        
-    }, 1000)
+    console.log("user connectd");
+    
 
     socket.on("message", (e) => {
         console.log(e.toString());
+        console.log(e.toString() == "ping");
         
+
+        if(e.toString() == "ping") {
+            socket.send("pong");
+        }
+
     })
 
 
