70123eac9fcc62300d2fddeaa70a822fd923ca98
netizen-om <borisagarom@gmail.com>
2025-05-26 14:30:19 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8000 });
+wss.on("connection", (socket) => {
+});
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port : 8000 })
+
+wss.on("connection", (socket) => {
+    console.log("hello");
+    socket.send("Hello, thx for connecting")
+})
