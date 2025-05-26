af5461fb140a0c8d8acd32b9578d1c94d0eb81c0
netizen-om <borisagarom@gmail.com>
2025-05-26 14:30:51 +0530

    added connection event handler

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ const wss = new WebSocketServer({ port : 8000 })
 
 wss.on("connection", (socket) => {
     console.log("hello");
-    socket.send("Hello, thx for connecting")
+    socket.send("Hello, thx for connecting");
+    
 })
